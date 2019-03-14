ca220df8c2a7de58b39d017e97f7b9d742b19bfc
Alex Dubov <dubovermail@gmail.com>
2019-03-14 12:16:47 -0500

    First and Final

---
+++ Assets/Scripts/end.cs
@@ -0,0 +1,25 @@
+﻿    using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement; // This is very important if we want to restart the level
+
+public class end : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+    void OnTriggerEnter(Collider other) {
+     if (other.transform.tag == "main") {
+         SceneManager.LoadScene("end");
+     }
+    }
+}
